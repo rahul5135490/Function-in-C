f69bca1679de0c86696df9fa919e1e7ce3b4430f
Rahul Srivastava <97574477+rahul5135490@users.noreply.github.com>
2023-10-16 22:16:40 +0530

    Add files via upload

---
+++ function1.cpp
@@ -0,0 +1,15 @@
+// Defnition of function-
+#include<stdio.h>
+#include<conio.h>
+void abc()
+{
+	printf("A");
+	printf("B");
+}
+main()
+{
+	abc();
+	abc();
+	abc();
+	getch();
+}
---
+++ function2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void abc()
+{
+	printf("A");
+	
+}
+void xyz()
+{
+	printf("B");
+	
+}
+main()
+{
+	abc();
+	xyz();
+	getch();
+}
