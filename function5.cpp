47ce85853db941b7c37968927a41334f85a25f2d
Rahul Srivastava <97574477+rahul5135490@users.noreply.github.com>
2023-10-17 04:09:57 +0530

    Add files via upload

---
+++ function5.cpp
@@ -0,0 +1,17 @@
+// second logic if you are implementing loss the you have to use second case
+// second case
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	void abc(void);// this will takes almost zero memory
+	abc();
+	abc();
+	getch();
+	
+}
+void abc()
+{
+	printf("Rahul Srivastava");
+}
+// here memory will use very less
---
+++ function6.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void abc()
+{
+	printf("Rahul");
+	
+}
+void xyz()
+{
+	printf("Srivastava");
+	abc();
+}
+main()
+{
+	abc();
+	xyz();
+	getch();
+}
