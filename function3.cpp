e25d63f0632631ece021c2c0f3f1a9c1d92cf28f
Rahul Srivastava <97574477+rahul5135490@users.noreply.github.com>
2023-10-17 04:09:08 +0530

    Add files via upload

---
+++ function3.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	abc();
+	abc();
+	getch();
+	}
+	void abc()
+	{
+		printf("Rahul")
+	}
+	// if you create main at first and function create down
+	// then it will throws some error
+	
---
+++ function4.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void abc();
+void xyz();
+main()
+{
+	abc();
+	xyz();
+	getch();
+}
+void abc()
+{
+	printf("Rahul");
+}
+void xyz()
+{
+	printf("Srivastava");
+}
+// for making function below we have to write decleration on top
